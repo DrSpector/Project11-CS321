1b5002d147dd59cff913f08762eaf836b9303493
DrSpector <louis.spector@gmail.com>
2023-10-15 18:48:48 -0400

    Created Junit tests

--- DataEntryTest.java
+++ DataEntryTest.java
@@ -4,23 +4,40 @@ import static org.junit.Assert.*;
 import java.util.*;
 import org.junit.Test;
 
-class DataEntryTest {
+public class DataEntryTest {
 
+	Immigrant[] ImmArray = new Immigrant[]{new Immigrant("Jerry","Smith","86ADF"),
+			new Immigrant("Waldo","Warenton","32A65"),
+			new Immigrant("Patrick","Guzman","55Y4A")	
+			};
 	
-	public static void main(String[] args)
+	
+	//@BeforeClass
+	//public static void main(String[] args)
 	{
-		org.junit.runner.JUnitCore.main("DataEntryTest");
+		Database.setImmigrant(new Immigrant("Jerry","Smith","86ADF"));
+		Database.setImmigrant(new Immigrant("Waldo","Warenton","32A65"));
+		Database.setImmigrant(new Immigrant("Patrick","Guzman","55Y4A"));
 		
-		
-		
-		
+
 	}
 	
 	
-	@Test (timeout = 3000) public void constructor1()
+	@Test
+	public void getInfo1()
 	{
-		assertEquals();
+		assertEquals(Database.getImmigrant("Jerry","Smith","86ADF"),ImmArray[0]);
+		assertEquals(Database.getImmigrant("Waldo","Warenton","32A65"),ImmArray[1]);
+		assertEquals(Database.getImmigrant("Patrick","Guzman","55Y4A"),ImmArray[2]);
 		
+	}
+	
+	@Test
+	public void getInfo2()
+	{
+		assertNotEquals(Database.getImmigrant("Jorry","Smith","86ADF"),ImmArray[0]);
+		assertNotEquals(Database.getImmigrant("Waldo","Warennton","32A65"),ImmArray[1]);
+		assertNotEquals(Database.getImmigrant("Patrick","Guzman","55B4A"),ImmArray[2]);
 		
 	}
 
