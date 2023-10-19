89f03ac8df3d09941a5c9be2a8b659d15f87ccdc
DrSpector <146766835+DrSpector@users.noreply.github.com>
2023-10-18 23:29:17 -0400

    Added comments and additional tests

--- WorkflowTableTest.java
+++ WorkflowTableTest.java
@@ -15,43 +15,51 @@ public class WorkflowTableTest {
 		
 		org.junit.runner.JUnitCore.main("DataEntryTest");
 	}
-	
+
+	//Initialize some Immigrant Objects to test with.
 	@Before
 	public void testInit()
 	{
-		wft = new WorkflowTable();
+		
 		a = new Immigrant("Jeff","Stevens","A54f6");
 		b = new Immigrant("Waldo","Warrenton","GF4TR");
 		
 	}
-	
+
+	//Constructor
 	@Test
-	public void Immigrant()
+	public void WorkflowTable()
 	{
+		wft = new WorkflowTable();
 		assertNotNull(wft);
 	}
-	
+
+	//setNext returns an ID
 	@Test
 	public void setNext1()
 	{
 		assertEquals(1,wft.setNext(a));
 	}
-	
+
+	//setNext returns -1 if given a bad item to add
 	@Test
 	public void setNext2()
 	{
 		assertNotEquals(-1,wft.setNext(null));
 	}
-	
+
+	//Peeking at wft should return a
 	@Test
 	public void getFirst1()
 	{
+		
 		assertEquals(a,wft.getFirst());
 	}
-		
+	//Peeking at wft should not return b
 	@Test
 	public void getFirst2()
 	{
+		wft.setNext(b);
 		assertNotEquals(b,wft.getFirst());
 	}
 	
@@ -62,27 +70,36 @@ public class WorkflowTableTest {
 		assertEquals(a,wft.getFirst());
 	}
 	
-	
+	//Popping wft should return a. a should then no longer be in wft
 	@Test
 	public void removeFirst1()
 	{
 		assertEquals(a, wft.removeFirst());
 		assertNotEquals(a,wft.getFirst());
 		
-		wft.setNext(a);
 	}
-	
+
+	//Popping wft should now return b. a should then no longer be in wft
 	@Test
 	public void removeFirst2()
 	{
-		assertNotEquals(b,wft.removeFirst());
+		assertEquals(b,wft.removeFirst());
+		assertNotEquals(b,wft.getFirst());
 	}
 	
-	public void removeFirst3()
+	//wft should be empty, null should be returned when peeked.
+	@Test
+	public void getFirst4()
+	{
+		assertNull(wft.getFirst());
+	}
+
+	//wft should be empty, null should be returned when popped.
+	@Test
+	public void removeFirst4()
 	{
 		assertNull(wft.removeFirst());
 	}
-	
 	
 	
 }
